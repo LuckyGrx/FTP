497446d19c197967e6801b5c119746771522ae26
LuckyGrx <LuckyGrx@gmail.com>
2019-05-05 15:29:24 +0800

    update stress test method

---
+++ concurrent_test/stress_test.c
@@ -0,0 +1,214 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#define ONE_BODY_MAX 1024
+
+#pragma pack(1)
+typedef struct request_pkg_head {
+	unsigned short body_len;   // 包体长度
+	unsigned short pkg_type;   // 报文类型
+}request_pkg_head_t;
+#pragma pack()
+
+enum type {
+	command_puts,
+	file_content,
+	end_file
+};
+
+#pragma pack(1)
+typedef struct response_pkg_head {
+	unsigned short body_len;         // 包体长度
+	unsigned short pkg_type;         // 报文类型
+	unsigned short handle_result;    // 结果码
+}response_pkg_head_t;
+#pragma pack()
+
+enum response {
+	response_success,
+	response_failed
+};
+
+int get_file_size(int filefd) {
+	struct stat statbuf;
+	fstat(filefd, &statbuf);
+	return statbuf.st_size;
+}
+
+int sendfile_by_mmap(int sockfd, int filefd) {
+	int file_size = get_file_size(filefd);
+	char* file_mmap=(char*)mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, filefd, 0);
+	char* p = file_mmap;
+	int one_send_size = ONE_BODY_MAX;
+	int remain = file_size;
+	int ret;
+	request_pkg_head_t pkg_head;
+	while (remain > 0) {
+		bzero(&pkg_head, sizeof(pkg_head));
+		pkg_head.pkg_type = htons(file_content);
+		if (remain < one_send_size)
+			pkg_head.body_len = htons(remain);
+		else
+			pkg_head.body_len = htons(one_send_size);
+		
+		// 发送包头
+		send(sockfd, (char*)&pkg_head, sizeof(pkg_head), 0);
+		// 发送包体
+		if (remain < one_send_size)
+			send(sockfd, file_mmap, remain, 0);
+		else
+			send(sockfd, file_mmap, one_send_size, 0);
+		
+		file_mmap += one_send_size;
+		remain -= one_send_size;
+	}
+
+	ret = munmap(p, file_size);
+	if (-1 == ret) {
+		perror("munmap");
+		return -1;
+	}
+
+	bzero(&pkg_head, sizeof(pkg_head));
+	pkg_head.pkg_type = htons(end_file);
+	// 发送文件结束标志
+	send(sockfd, (char*)&pkg_head, sizeof(pkg_head), 0);
+}
+
+int request_control_puts(int sockfd, char* file_name) {
+	static int file_index = 1;
+	request_pkg_head_t pkg_head;
+	bzero(&pkg_head, sizeof(pkg_head));
+
+	char file_name_send[15];
+	strcpy(file_name_send, file_name);
+
+	sprintf(file_name_send, "%s%d", file_name, file_index++);
+	printf("%s\n", file_name_send);
+
+	pkg_head.pkg_type = htons(command_puts);
+	pkg_head.body_len = htons(strlen(file_name_send));
+	int filefd;
+	if ((filefd = open(file_name, O_RDONLY)) == -1) {
+		perror("open");
+		return -1;
+	}
+
+	send(sockfd, (char*)&pkg_head, sizeof(pkg_head), 0);
+	send(sockfd, file_name_send, strlen(file_name_send), 0);
+	sendfile_by_mmap(sockfd, filefd);
+	close(filefd);
+}
+
+int response_control_puts(response_pkg_head_t* response_pkg_head) {
+	printf("%s\n", __func__);
+	unsigned short handle_result = ntohs(response_pkg_head->handle_result);
+	if (handle_result == response_success)
+		printf("上传成功\n");
+	else if (handle_result == response_failed)
+		printf("上传失败\n");
+}
+
+int response_control(int sockfd) {
+	response_pkg_head_t response_pkg_head;
+	bzero(&response_pkg_head, sizeof(response_pkg_head));
+	recv(sockfd, (char*)&response_pkg_head, sizeof(response_pkg_head_t), 0);
+	unsigned short command_type = ntohs(response_pkg_head.pkg_type);
+
+	switch (command_type) {
+		case command_puts:
+			response_control_puts(&response_pkg_head);
+			break;
+		default:
+			break;
+	}
+}
+
+int setnonblocking(int fd) {
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+
+void addfd(int epoll_fd, int fd) {
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLOUT | EPOLLET | EPOLLERR;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
+
+// 向服务器发起num个TCP连接,我们可以通过改变num来调整测试压力
+void start_conn(int epoll_fd, int num, const char* ip, int port) {
+    int ret = 0;
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port);
+
+    for (int i = 0; i < num; ++i) {
+        int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+        printf("create 1 sock\n");
+        if (sockfd < 0)
+            continue;
+        if (connect(sockfd, (struct sockaddr*)&address, sizeof(address)) == 0) {
+            printf("build connection %d\n", i);
+            addfd(epoll_fd, sockfd);
+        }
+    }
+}
+
+void close_conn(int epoll_fd, int sockfd) {
+    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, sockfd, 0);
+    close(sockfd);
+}
+
+
+int main(int argc, char* argv[]) {
+    assert(argc == 5);
+    int epoll_fd = epoll_create(1);
+    start_conn(epoll_fd, atoi(argv[3]), argv[1], atoi(argv[2]));
+    struct epoll_event events[10000];
+
+    while (1) {
+        int fds = epoll_wait(epoll_fd, events, 10000, -1);
+        for (int i = 0; i < fds; i++) {
+            int sockfd = events[1].data.fd;
+
+            if (events[i].events & EPOLLIN) {
+
+                response_control(sockfd);
+
+                struct epoll_event event;
+                event.events = EPOLLOUT | EPOLLET | EPOLLERR;
+                event.data.fd = sockfd;
+                epoll_ctl(epoll_fd, EPOLL_CTL_MOD, sockfd, &event);
+
+            } else if (events[i].events & EPOLLOUT) {
+
+                request_control_puts(sockfd, argv[4]);
+
+                struct epoll_event event;
+                event.events = EPOLLIN | EPOLLET | EPOLLERR;
+                event.data.fd = sockfd;
+                epoll_ctl(epoll_fd, EPOLL_CTL_MOD, sockfd, &event);
+            } else if (events[i].events & EPOLLERR) {
+                close_conn(epoll_fd, sockfd);
+            }
+        }
+    }
+    return 0;
+}
